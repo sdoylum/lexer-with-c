567cbe66fbe08a0224732a5c3f3bd530aff8f919
sdoylum <60060926+sdoylum@users.noreply.github.com>
2020-11-14 02:15:49 +0300

    Add files via upload

--- lexer.c
+++ lexer.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define T_PLUS PLUS
+#define T_MINUS MINUS
+
 
 struct Token{
 	char * type;
@@ -19,30 +22,75 @@ struct Lexer{
 	char currentChar;
 };
 
-void advance(struct Lexer lex){
-	lex.position++;
-	if(strlen(lex.text) > lex.position){
-		lex.currentChar = lex.text[lex.position];
-		
+void advance(struct Lexer *lex){
+	lex->position++;
+	if(strlen(lex->text) > lex->position){
+		lex->currentChar = lex->text[lex->position];	
 	}else{
-		lex.currentChar = '\0';
+		lex->currentChar = '\0';
 	}
 }
 
-void tokener(struct Lexer lex, struct Node *lis){
-	printf("%d",lex.currentChar);
-	while(lex.currentChar != '8'){
-
-		if(lex.currentChar == '+'){
+void tokener(struct Lexer *lex, struct Node *lis){
+	while(lex->currentChar != '\0'){
+		if(lex->currentChar == '+'){
 			lis -> next = (struct Node*)malloc(sizeof(struct Node));
-			lis -> next -> tok.type="art";
-			lis -> next -> tok.value="+";
-			printf("%s", lis->tok.value);
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="topla";
 			lis = lis->next;
 			lis->next = NULL;
+			printf("%s", lis->tok.value);
 			advance(lex);
-		}else{
-			
+		}else if(lex->currentChar == '-'){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="cikar";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		}else if(lex->currentChar == '*'){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="carp";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		}else if(lex->currentChar == '/'){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="bol";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		}else if(lex->currentChar == '='){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="equals";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		}else if(lex->currentChar == ';'){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="semicolon";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		}else if(lex->currentChar == '.'){
+			lis -> next = (struct Node*)malloc(sizeof(struct Node));
+			lis -> next -> tok.type="op";
+			lis -> next -> tok.value="dot";
+			lis = lis->next;
+			lis->next = NULL;
+			printf("%s", lis->tok.value);
+			advance(lex);
+		
+		}else{	
 			advance(lex);
 		}
 	}
@@ -52,14 +100,16 @@ void tokener(struct Lexer lex, struct Node *lis){
 int main()
 {
 	struct Node *list = (struct Node*)malloc(sizeof(struct Node));
-	struct Lexer lex;
+	struct Lexer *lex = (struct Lexer*)malloc(sizeof(struct Lexer));;
 	list->tok.type="idf";
 	list->tok.value="cc";
 	list->next=NULL;
-	lex.position = -1;
-	scanf("%s", lex.text);
+	lex->position = -1;
+	lex->text = (char*) malloc(256*sizeof(char));
+	scanf("%s", lex->text);
 	advance(lex);
 	tokener(lex, list);
+	
 	return 0;
 }
 
